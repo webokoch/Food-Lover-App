4702a2a84171551cd6a79b4d6b87ff592b96c8b6
Stefan Koch <webokoch@gmx.de>
2022-03-25 14:26:15 +0100

    added tests for Food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,9 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a name"
-  it "is not valid without a cuisine"
-  it "is not valid without a description"
-  it "is not valid without a user"
+  current_user = User.first_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+
+  subject{
+    described_class.new(name: 'Sushi',
+                        cuisine: 'Japanese',
+                        description: 'A staple rice dish, consisting of cooked rice flavoured with vinegar and a variety of vegetable, egg, or raw seafood garnishes and served cold.',
+                        user: current_user)
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid  
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a cuisine" do
+    subject.cuisine = nil 
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil 
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a user" do
+    subject.user = nil 
+    expect(subject).to_not be_valid
+  end
 end
