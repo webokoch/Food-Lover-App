1ea75523e5edd9969936fcd77361e22def11c8b9
Stefan Koch <webokoch@gmx.de>
2022-03-26 17:01:35 +0100

    added initial actions to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,51 @@
 class FoodsController < ApplicationController
+  before_action :find_food, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:home, :show]
+
   def index
     @foods = Food.all
+  end
+
+  def show
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      redirect_to food_path(@food)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to food_path(@food)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  private
+
+  def find_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :cuisine, :description)
   end
 end
