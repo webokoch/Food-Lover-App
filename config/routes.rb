427b908ffde7465b0f0fb40292ab47660badf0d2
Stefan Koch <webokoch@gmx.de>
2022-03-28 22:32:09 +0200

    added route for foods#users_foods

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :foods
+  get "/users_foods", to: 'foods#users_foods'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
