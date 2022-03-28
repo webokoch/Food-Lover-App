220c148551346856b91978c8835a8ff002049575
Stefan Koch <webokoch@gmx.de>
2022-03-28 22:33:23 +0200

    updated food & user model

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,5 +2,5 @@ class Food < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :cuisine, presence: true
   validates :description, presence: true, length: { maximum: 500 }
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
 end
