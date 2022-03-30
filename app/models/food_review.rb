7b803c076de04ea7e689fd320e2b12f6ebae5e79
Stefan Koch <webokoch@gmx.de>
2022-03-30 14:09:50 +0200

    added validations for food_review model

--- app/models/food_review.rb
+++ app/models/food_review.rb
@@ -1,3 +1,5 @@
 class FoodReview < ApplicationRecord
+  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :content, presence: true, length: { maximum: 500 } 
   belongs_to :food
 end
