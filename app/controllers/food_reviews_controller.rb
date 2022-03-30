3f7edcd5550155e3d6568ab1a0cab60f8fd8dfe7
Stefan Koch <webokoch@gmx.de>
2022-03-30 14:27:48 +0200

    added food_reviews#new action and view

--- app/controllers/food_reviews_controller.rb
+++ app/controllers/food_reviews_controller.rb
@@ -1,2 +1,20 @@
 class FoodReviewsController < ApplicationController
+  before_action :find_food
+
+  def new
+    @food_review = FoodReview.new
+  end
+
+  def create
+
+  end
+
+  private
+  def find_food
+    @food = Food.find(params[:food_id])
+  end
+
+  def food_review_params
+    params.require(:food_review).permit(:rating, :content)
+  end
 end
