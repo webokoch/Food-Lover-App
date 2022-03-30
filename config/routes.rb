3c509383609b281bf4e4f9e225b97ce0b7b9c08b
Stefan Koch <webokoch@gmx.de>
2022-03-30 14:17:21 +0200

    added routes for food_reviews#new and #create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :foods
+  resources :foods do
+    resources :food_reviews, only: [ :new, :create ]
+  end
+
   get "/users_foods", to: 'foods#users_foods'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
