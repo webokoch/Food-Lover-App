d15a9e7a9c5a40a26353a6bba1f3287ec55b167f
Stefan Koch <webokoch@gmx.de>
2022-04-04 21:37:29 +0200

    added restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_113736) do
+ActiveRecord::Schema.define(version: 2022_04_04_193617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 2022_03_30_113736) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "cuisine"
+    t.string "location"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -70,4 +81,5 @@ ActiveRecord::Schema.define(version: 2022_03_30_113736) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "food_reviews", "foods"
   add_foreign_key "foods", "users"
+  add_foreign_key "restaurants", "users"
 end
