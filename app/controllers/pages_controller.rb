a672e576efd7883dd078093d8b46d46b861b6628
Stefan Koch <webokoch@gmx.de>
2022-04-05 15:18:08 +0200

    changed pages controller to handle multisearch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   end
 
   def search
-    @foods = Food.search_foods(query_params[:query])
+    @results = PgSearch.multisearch(query_params[:query])
   end
 
   private
