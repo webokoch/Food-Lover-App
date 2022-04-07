88a15d6099f734b5afa9c3a54d83d89f998ec01f
Stefan Koch <webokoch@gmx.de>
2022-04-07 18:33:17 +0200

    added action an view for FoodRestaurants#create

--- app/controllers/food_restaurants_controller.rb
+++ app/controllers/food_restaurants_controller.rb
@@ -1,10 +1,17 @@
 class FoodRestaurantsController < ApplicationController
   before_action :set_food
 
+  def new
+    @food_restaurant = FoodRestaurant.new
+  end
+  
   def create
-    @food_restaurant = FoodRestaurant.new(food: @food, food_restaurant_params)
-    @food_restaurant.user = current_user
-    @food_restaurant.save
+    @food_restaurant = FoodRestaurant.new(food_id: params[:food_id], restaurant_id: food_restaurant_params[:restaurant_id])
+    if @food_restaurant.save
+      redirect_to food_path(@food)
+    else
+      render :new
+    end
   end
 
   private
@@ -14,6 +21,6 @@ class FoodRestaurantsController < ApplicationController
   end
 
   def food_restaurant_params
-    params.require(:food_restaurant).permit(restaurant_id:)
+    params.require(:food_restaurant).permit(:restaurant_id)
   end
 end
--- app/models/food_restaurant.rb
+++ app/models/food_restaurant.rb
@@ -1,5 +1,4 @@
 class FoodRestaurant < ApplicationRecord
   belongs_to :food
   belongs_to :restaurant
-  belongs_to :user
 end
