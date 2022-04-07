1e1c0999341eaecc5b7800ed6011d93759236508
Stefan Koch <webokoch@gmx.de>
2022-04-07 14:00:05 +0200

    first draft for FoodRestaurants#create action

--- app/controllers/food_restaurants_controller.rb
+++ app/controllers/food_restaurants_controller.rb
@@ -1,2 +1,19 @@
 class FoodRestaurantsController < ApplicationController
+  before_action :set_food
+
+  def create
+    @food_restaurant = FoodRestaurant.new(food: @food, food_restaurant_params)
+    @food_restaurant.user = current_user
+    @food_restaurant.save
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:food_id])
+  end
+
+  def food_restaurant_params
+    params.require(:food_restaurant).permit(restaurant_id:)
+  end
 end
--- app/models/food_restaurant.rb
+++ app/models/food_restaurant.rb
@@ -1,4 +1,5 @@
 class FoodRestaurant < ApplicationRecord
   belongs_to :food
   belongs_to :restaurant
+  belongs_to :user
 end
