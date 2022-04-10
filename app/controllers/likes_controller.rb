52837d11691827e5473b117b3e8f1c0585ca1879
Stefan Koch <webokoch@gmx.de>
2022-04-10 21:07:56 +0200

    added create action to Likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  before_action :find_food
+
+  def create
+    @food.likes.create(user_id: current_user.id)
+    redirect_to food_path(@food)
+  end
+
+  private
+
+  def find_post
+    @food = Food.find(params[:food_id])
+  end
 end
