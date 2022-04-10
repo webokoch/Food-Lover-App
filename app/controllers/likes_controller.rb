6021c0f784495eb2dcebecfc146ba997149ee937
Stefan Koch <webokoch@gmx.de>
2022-04-10 21:11:01 +0200

    added function to only like a food once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,22 @@ class LikesController < ApplicationController
   before_action :find_food
 
   def create
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
     @food.likes.create(user_id: current_user.id)
     redirect_to food_path(@food)
+    end
   end
 
   private
 
   def find_post
     @food = Food.find(params[:food_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, food_id:
+    params[:food_id]).exists?
   end
 end
