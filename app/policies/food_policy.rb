473ef1309b9d73a92c1072c47ba7eff043b8db8d
Stefan Koch <webokoch@gmx.de>
2022-04-12 18:29:51 +0200

    added authorization foods#destroy and changed show

--- app/policies/food_policy.rb
+++ app/policies/food_policy.rb
@@ -5,11 +5,19 @@ class FoodPolicy < ApplicationPolicy
     end
   end
 
+  def show?
+    true
+  end
+
   def create?
     true
   end
 
   def update?
+    user == record.user
+  end
+
+  def destroy?
     user == record.user
   end
 end
