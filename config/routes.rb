50225cde58e1ab7d9903d70592c70a61a483b773
Stefan Koch <webokoch@gmx.de>
2022-04-14 14:31:08 +0200

    added route for Foods#show API

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :foods, only: [ :index ]
+      resources :foods, only: [ :index, :show ]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
