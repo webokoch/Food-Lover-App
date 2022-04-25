5ad5c95f95176bdc7c5cacb75a109058e592a75f
Stefan Koch <webokoch@gmx.de>
2022-04-25 19:09:54 +0200

    added address to Restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_185906) do
+ActiveRecord::Schema.define(version: 2022_04_25_170827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2022_04_10_185906) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
