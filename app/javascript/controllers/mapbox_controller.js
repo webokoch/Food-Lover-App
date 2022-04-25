a024b2e02f4d78f20b56fe261187891c15989d4b
Stefan Koch <webokoch@gmx.de>
2022-04-25 19:52:46 +0200

    set up mapbox controller with markers

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -11,8 +11,24 @@ export default class extends Controller {
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+        container: this.element,
+        style: "mapbox://styles/mapbox/streets-v10"
     })
+    this._addMarkersToMap()
+    this._fitMapToMarkers()
   }
+
+    _addMarkersToMap() {
+        this.markersValue.forEach((marker) => {
+        new mapboxgl.Marker()
+            .setLngLat([ marker.lng, marker.lat ])
+            .addTo(this.map)
+        });
+    }
+
+    _fitMapToMarkers() {
+        const bounds = new mapboxgl.LngLatBounds()
+        this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+        this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+      }
 }
