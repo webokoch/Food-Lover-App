edab90d78e6ac592dfb44afa6bf6b2634bd565ad
Stefan Koch <webokoch@gmx.de>
2022-04-25 19:15:38 +0200

    added coordinates to Restaurant for geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_25_170827) do
+ActiveRecord::Schema.define(version: 2022_04_25_171451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,8 @@ ActiveRecord::Schema.define(version: 2022_04_25_170827) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
