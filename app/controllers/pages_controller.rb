fc1c7d770c34e9e74c2fbb7808eda19fdc75dd18
Stefan Koch <webokoch@gmx.de>
2022-04-26 18:21:34 +0200

    added info window to markers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,8 @@ class PagesController < ApplicationController
     @markers = @restaurants.geocoded.map do |restaurant|
       {
         lat: restaurant.latitude,
-        lng: restaurant.longitude
+        lng: restaurant.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { restaurant: restaurant })
       }
     end
 
--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -12,23 +12,23 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
         container: this.element,
-        style: "mapbox://styles/mapbox/streets-v10"
+        style: "mapbox://styles/mapbox/streets-v10",
+        center:[
+          13.409419,
+          52.520815
+        ],
+        zoom: 12
     })
     this._addMarkersToMap()
-    this._fitMapToMarkers()
   }
 
     _addMarkersToMap() {
         this.markersValue.forEach((marker) => {
-        new mapboxgl.Marker()
-            .setLngLat([ marker.lng, marker.lat ])
-            .addTo(this.map)
-        });
-    }
-
-    _fitMapToMarkers() {
-        const bounds = new mapboxgl.LngLatBounds()
-        this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-        this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+          const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+          new mapboxgl.Marker()
+              .setLngLat([ marker.lng, marker.lat ])
+              .setPopup(popup)
+              .addTo(this.map)
+          });
       }
 }
