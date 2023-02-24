12b0cbae0b820e1625bff3778fe4cffd722b6c5b
Stefan Koch <webokoch@gmx.de>
2023-02-24 12:11:25 +0100

    added new & create method for restaurant_reviews

--- app/controllers/restaurant_reviews_controller.rb
+++ app/controllers/restaurant_reviews_controller.rb
@@ -1,2 +1,29 @@
 class RestaurantReviewsController < ApplicationController
+  before_action :find_restaurant
+
+  def new
+    @restaurant_review = RestaurantReview.new
+    authorize @restaurant_review
+  end
+
+  def create
+    @restaurant_review = RestaurantReview.new(restaurant_review_params)
+    @restaurant_review.restaurant = @restaurant
+    @restaurant_review.user = current_user
+    authorize @restaurant_review
+    if @restaurant_review.save
+      redirect_to restaurant_path(@restaurant, anchor: "restaurant-review-#{@restaurant_review.id}")
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def restaurent_review_params
+    params.require(:restaurant_review).permit(:rating, :content)
+  end
 end
