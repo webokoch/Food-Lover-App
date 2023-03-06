7a2c5af549c3fe96f94017cce719bd392b09a448
Stefan Koch <webokoch@gmx.de>
2023-03-06 14:12:42 +0100

    added route for api page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "/food_info", to: 'pages#food_info'
   get "/restaurant_info", to: 'pages#restaurant_info'
   get "/map_info", to: 'pages#map_info'
+  get "/api", to: 'pages#api'
   
   resources :foods do
     resources :food_reviews, only: [ :new, :create ]
